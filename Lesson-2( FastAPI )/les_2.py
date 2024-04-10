11187605630a1b2322a962513df61aeb6bff7328
IvanSlivenko <ivan8822@ukr.net>
2024-04-10 21:51:45 +0300

    10-04-2024-21-51

--- Lesson-2( FastAPI )/les_2.py
+++ Lesson-2( FastAPI )/les_2.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
 
